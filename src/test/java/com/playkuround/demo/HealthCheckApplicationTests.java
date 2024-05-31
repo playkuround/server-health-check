c17281f1a13e3ebecf1d49c1a23072e9773abe91
seungtaekhong <hsk4991149@naver.com>
2024-05-31 18:15:28 +0900

    initial project

---
+++ src/test/java/com/playkuround/demo/HealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playkuround.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
