588a9c859877343b4f777deb1da8b4878782e912
seungtaekhong <hsk4991149@naver.com>
2024-05-31 19:04:51 +0900

    feat: login interceptor

---
+++ src/main/java/com/playkuround/demo/global/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.playkuround.demo.global.config;
+
+import com.playkuround.demo.global.interceptor.LoginCheckInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+}
---
+++ src/main/java/com/playkuround/demo/global/interceptor/SessionConst.java
@@ -0,0 +1,5 @@
+package com.playkuround.demo.global.interceptor;
+
+public enum SessionConst {
+    LOGIN_MEMBER
+}
