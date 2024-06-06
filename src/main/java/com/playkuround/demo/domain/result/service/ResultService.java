70fba4743c7c2bf416c95664879f5b9699dc8d18
seungtaekhong <hsk4991149@naver.com>
2024-06-06 21:17:20 +0900

    feat: 로직을 service layer로 이동시키고 transactional 적용

--- src/main/java/com/playkuround/demo/domain/result/service/ResultService.java
+++ src/main/java/com/playkuround/demo/domain/result/service/ResultService.java
@@ -1,11 +1,23 @@
 package com.playkuround.demo.domain.result.service;
 
+import com.playkuround.demo.domain.common.FailCountThreshold;
+import com.playkuround.demo.domain.email.dto.Mail;
+import com.playkuround.demo.domain.email.service.EmailService;
 import com.playkuround.demo.domain.result.entity.Result;
 import com.playkuround.demo.domain.result.repository.ResultRepository;
+import com.playkuround.demo.domain.target.entity.Target;
+import com.playkuround.demo.domain.target.repository.TargetRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +25,55 @@ import java.util.List;
 public class ResultService {
 
     private final ResultRepository resultRepository;
+    private final TargetRepository targetRepository;
+    private final EmailService emailService;
 
     public List<Result> findByTargetAndDateSorted(Long targetId, LocalDate date) {
         return resultRepository.findByTargetAndDateSorted(targetId, date);
+    }
+
+    @Transactional
+    public void doHealthCheck() {
+        List<Target> targets = targetRepository.findAll();
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .setReadTimeout(Duration.ofSeconds(3))
+                .build();
+
+        List<Target> errorTargets = new ArrayList<>();
+        for (Target target : targets) {
+            int statusCode;
+            try {
+                ResponseEntity<String> response
+                        = restTemplate.getForEntity(target.getHealthCheckURL(), String.class);
+                statusCode = response.getStatusCode().value();
+            } catch (HttpClientErrorException e) {
+                statusCode = 400;
+            }
+
+            resultRepository.save(new Result(target, statusCode));
+            target.updateStatus(statusCode);
+
+            if (FailCountThreshold.isOverThreshold(target.getConsecutiveFailCount())
+                    && !target.isTodaySend()) {
+                errorTargets.add(target);
+            }
+        }
+
+        if (!errorTargets.isEmpty()) {
+            // TODO Template
+            String title = "Health Check Error";
+            StringBuilder contentBody = new StringBuilder();
+            contentBody.append("Health Check Error<br/>");
+            for (Target target : errorTargets) {
+                contentBody.append(target.getHost())
+                        .append(" >> ")
+                        .append(target.getHealthCheckURL())
+                        .append("<br/>");
+                target.setTodaySend(true);
+            }
+            Mail mail = new Mail(title, contentBody.toString());
+            emailService.sendMailAsync(mail);
+        }
     }
 }
--- src/main/java/com/playkuround/demo/global/scheduler/DynamicScheduler.java
+++ src/main/java/com/playkuround/demo/global/scheduler/DynamicScheduler.java
@@ -1,34 +1,24 @@
 package com.playkuround.demo.global.scheduler;
 
-import com.playkuround.demo.domain.common.FailCountThreshold;
-import com.playkuround.demo.domain.email.dto.Mail;
 import com.playkuround.demo.domain.email.service.EmailService;
 import com.playkuround.demo.domain.report.service.ReportService;
-import com.playkuround.demo.domain.result.entity.Result;
-import com.playkuround.demo.domain.result.repository.ResultRepository;
-import com.playkuround.demo.domain.target.entity.Target;
+import com.playkuround.demo.domain.result.service.ResultService;
 import com.playkuround.demo.domain.target.repository.TargetRepository;
 import lombok.Getter;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.scheduling.support.PeriodicTrigger;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class DynamicScheduler {
 
     private final EmailService emailService;
     private final TargetRepository targetRepository;
-    private final ResultRepository resultRepository;
+    private final ResultService resultService;
     private final ReportService reportService;
     private ThreadPoolTaskScheduler scheduler;
 
@@ -37,31 +27,24 @@ public class DynamicScheduler {
     @Getter
     private String cron;
 
-    public DynamicScheduler(TargetRepository targetRepository, ResultRepository resultRepository,
+    public DynamicScheduler(TargetRepository targetRepository, ResultService resultService,
                             ReportService reportService, EmailService emailService) {
         this.targetRepository = targetRepository;
-        this.resultRepository = resultRepository;
+        this.resultService = resultService;
         this.reportService = reportService;
         this.emailService = emailService;
-        this.cron = "0 0 23 * * ?";
         this.ms = 30000;
+        this.cron = "0 0 0 * * ?";
         startScheduler();
-        scheduler.schedule(getResetTargetSendTodayRunnable(), new CronTrigger("0 0 0 * * ?"));
-        scheduler.schedule(getDailyReportSaveRunnable(), new CronTrigger("0 0 0 * * ?"));
     }
 
     private void startScheduler() {
         scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(2);
         scheduler.initialize();
+        scheduler.schedule(getDailyReportSaveRunnable(), new CronTrigger(cron));
         scheduler.schedule(getHealthCheckRunnable(), new PeriodicTrigger(Duration.ofMillis(ms)));
-        scheduler.schedule(getSendEmailRunnable(), new CronTrigger(cron));
-    }
-
-    public void updateCron(String cron) {
-        scheduler.shutdown();
-        this.cron = cron;
-        startScheduler();
+        scheduler.schedule(getResetTargetSendTodayRunnable(), new CronTrigger("0 0 0 * * ?"));
     }
 
     public void updateMillisecond(int ms) {
@@ -71,59 +54,7 @@ public class DynamicScheduler {
     }
 
     private Runnable getHealthCheckRunnable() {
-        return () -> {
-            // TODO transactional
-            List<Target> targets = targetRepository.findAll();
-            RestTemplate restTemplate = new RestTemplateBuilder()
-                    .setConnectTimeout(Duration.ofSeconds(3))
-                    .setReadTimeout(Duration.ofSeconds(3))
-                    .build();
-
-            List<Target> errorTargets = new ArrayList<>();
-            for (Target target : targets) {
-
-                int statusCode;
-                try {
-                    ResponseEntity<String> response
-                            = restTemplate.getForEntity(target.getHealthCheckURL(), String.class);
-                    statusCode = response.getStatusCode().value();
-                } catch (HttpClientErrorException e) {
-                    statusCode = 400;
-                }
-
-                resultRepository.save(new Result(target, statusCode));
-                target.updateStatus(statusCode);
-                targetRepository.save(target);
-
-                if (FailCountThreshold.isOverThreshold(target.getConsecutiveFailCount())
-                        && !target.isTodaySend()) {
-                    errorTargets.add(target);
-                }
-            }
-
-            if (!errorTargets.isEmpty()) {
-                // TODO Template
-                String title = "Health Check Error";
-                StringBuilder sb = new StringBuilder();
-                sb.append("Health Check Error<br/>");
-                for (Target target : errorTargets) {
-                    sb.append(target.getHost())
-                            .append(" >> ")
-                            .append(target.getHealthCheckURL())
-                            .append("<br/>");
-                    target.setTodaySend(true);
-                }
-                Mail mail = new Mail(title, sb.toString());
-                emailService.sendMail(mail);
-                targetRepository.saveAll(errorTargets);
-            }
-        };
-    }
-
-    private Runnable getSendEmailRunnable() {
-        return () -> {
-
-        };
+        return resultService::doHealthCheck;
     }
 
     private Runnable getResetTargetSendTodayRunnable() {
